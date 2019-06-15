ed8c7ce512209e3476fcb3a9d4c487d149a26ca7
seoyeon <35373729+ytonyun@users.noreply.github.com>
2019-06-15 16:26:35 +0900

    layout
    .

--- Android Studio/app/src/main/java/com/obdh/outbackdoghouse/Info.java
+++ Android Studio/app/src/main/java/com/obdh/outbackdoghouse/Info.java
@@ -1,6 +1,18 @@
 package com.obdh.outbackdoghouse;
 
 import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class Info extends Activity {
+public class Info extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.info_layout, container, false);
+    }
 }
--- Android Studio/app/src/main/java/com/obdh/outbackdoghouse/MainActivity.java
+++ Android Studio/app/src/main/java/com/obdh/outbackdoghouse/MainActivity.java
@@ -1,14 +1,35 @@
 package com.obdh.outbackdoghouse;
 
-import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.loginmain_layout);
+        setContentView(R.layout.index_layout);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_recents:
+                        Toast.makeText(MainActivity.this, "Recents", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_favorites:
+                        Toast.makeText(MainActivity.this, "Favorites", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_nearby:
+                        Toast.makeText(MainActivity.this, "Nearby", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
     }
+
 }
